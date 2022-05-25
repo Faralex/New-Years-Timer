76b6c2a2428dfb0961e38d198e507be796bc71ca
Faralex <farakrikqq@mail.ru>
2022-05-25 19:31:53 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const timer = document.querySelector("[data-timer]");
+
+const countDownDate = new Date("Jan 1, 2023").getTime();
+
+const countdown = setInterval(function () {
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  timer.innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(countdown);
+    timer.innerHTML = "DONE";
+  }
+}, 1000);
+
+console.log(123);
